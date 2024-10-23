da462fc175e0546ebf6eb74e8b82665fda44a720
jahir <nockjahirul@gmail.com>
2024-10-23 19:39:59 +0600

    add country.jsx secrion

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,19 +1,29 @@
 import { useState } from "react";
 
- const Country = ({country}) => {
+ const Country = ({country, handleVisitedCountry}) => {
     const {name,flags,population,area,cca3}= country;
 
-    const [visited,setVisited]= useState(false)
+    const [visited,setVisited]= useState(false);
+
     const handleVisited = () =>{
         setVisited(!visited);
     }
+
+    //  const passWithParams =() =>{handleVisitedCountry(country)};
+       
+    // console.log( handleVisitedCountry)
+  
     return (
-        <div className='box'>
-            <h3>Name:{name.common}</h3>
+        <div className={`box ${visited ?'visited':'noneVisited'}`}>
+            <h3 style={{color:visited ? 'purple':'black'}}>{name.common}</h3>
             <img src={flags.png} alt="" />
             <p>Popilation:{population}</p>
             <p>area:{area}</p>
             <p>Code:{cca3}</p>
+
+            <button onClick={() =>handleVisitedCountry(country)}>Mark visited</button>
+            <br /><br />
+
             <button onClick={handleVisited}>{visited ? 'visited' : 'going'}</button>
             {
               visited ? ' I have visited this country.': 'I want to visit'  
